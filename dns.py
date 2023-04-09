a3365e4b1323552dc85ede4c1717612322a7ff45
hqy <hqyrex@umich.edu>
2023-04-09 17:48:02 -0400

    add main function

--- dns.py
+++ dns.py
@@ -8,12 +8,12 @@ ip = "127.0.0.1"
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip, port))
 
-
-while True:  # can be improved by adding another thread to control the socket
-    data, addr = sock.recvfrom(512)
-    manager = dnsManager(data, "zones/")
-    sys.stdout.buffer.write(data)  # use this to output like in wireshark
-    r = manager.build_response()
-    sys.stdout.buffer.write(r)
-    # exit()
-    sock.sendto(r, addr)
+if __name__ == "__main__":
+    while True:  # can be improved by adding another thread to control the socket
+        data, addr = sock.recvfrom(512)
+        manager = dnsManager(data, "zones/")
+        sys.stdout.buffer.write(data)  # use this to output like in wireshark
+        r = manager.build_response()
+        sys.stdout.buffer.write(r)
+        # exit()
+        sock.sendto(r, addr)
